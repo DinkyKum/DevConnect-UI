29eda8c15a12a6abddb5a57e61d1704ae896f075
DinkyKum <dinky2912@gmail.com>
2025-02-02 23:21:51 +0530

    Changes

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import EditProfile from './editProfile';
+import EditProfile from './EditProfile';
 
 const Profile = () => {
   const user= useSelector((store)=>store.user);
